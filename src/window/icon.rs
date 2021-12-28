f567da70e14fbc9d30708729ce161da5f84731a3
daladim <daladim@users.noreply.github.com>
2021-12-29 00:15:18 +0100

    Added convenience functions for window::icon::Icon

--- src/window/icon.rs
+++ src/window/icon.rs
@@ -1,6 +1,7 @@
 //! Attach an icon to the window of your application.
 use std::fmt;
 use std::io;
+use std::path::Path;
 
 /// The icon of a window.
 #[cfg(not(target_arch = "wasm32"))]
@@ -35,6 +36,39 @@ impl Icon {
     ) -> Result<Self, Error> {
         Ok(Icon)
     }
+
+    /// Creates an icon from an image file.
+    ///
+    /// This will return an error in case the file is missing at run-time. You may prefer [`Self::from_file_data`] instead.
+    #[cfg(feature = "image_rs")]
+    pub fn from_file<P: AsRef<Path>>(icon_path: P) -> Result<Self, Error> {
+        let icon = image_rs::io::Reader::open(icon_path)?.decode()?.to_rgba8();
+
+        Self::from_rgba(icon.to_vec(), icon.width(), icon.height())
+    }
+
+    /// Creates an icon from the content of an image file.
+    ///
+    /// This content can be included in your application at compile-time, e.g. using the `include_bytes!` macro. \
+    /// You can pass an explicit file format. Otherwise, the file format will be guessed at runtime.
+    #[cfg(feature = "image_rs")]
+    pub fn from_file_data(
+        data: &[u8],
+        explicit_format: Option<image_rs::ImageFormat>,
+    ) -> Result<Self, Error> {
+        let mut icon = image_rs::io::Reader::new(std::io::Cursor::new(data));
+        let icon_with_format = match explicit_format {
+            Some(format) => {
+                icon.set_format(format);
+                icon
+            }
+            None => icon.with_guessed_format()?,
+        };
+
+        let pixels = icon_with_format.decode()?.to_rgba8();
+
+        Self::from_rgba(pixels.to_vec(), pixels.width(), pixels.height())
+    }
 }
 
 /// An error produced when using `Icon::from_rgba` with invalid arguments.
@@ -60,6 +94,16 @@ pub enum Error {
 
     /// The underlying OS failed to create the icon.
     OsError(io::Error),
+
+    /// The `image` crate reported an error
+    #[cfg(feature = "image_rs")]
+    ImageError(image_rs::error::ImageError),
+}
+
+impl From<std::io::Error> for Error {
+    fn from(os_error: std::io::Error) -> Self {
+        Error::OsError(os_error)
+    }
 }
 
 #[cfg(not(target_arch = "wasm32"))]
@@ -93,6 +137,13 @@ impl From<Icon> for iced_winit::winit::window::Icon {
     }
 }
 
+#[cfg(feature = "image_rs")]
+impl From<image_rs::error::ImageError> for Error {
+    fn from(image_error: image_rs::error::ImageError) -> Self {
+        Self::ImageError(image_error)
+    }
+}
+
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
@@ -121,6 +172,10 @@ impl fmt::Display for Error {
                 icon: {:?}",
                 e
             ),
+            #[cfg(feature = "image_rs")]
+            Error::ImageError(e) => {
+                write!(f, "Unable to create icon from a file: {:?}", e)
+            }
         }
     }
 }
